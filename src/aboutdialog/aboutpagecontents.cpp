b3f25ef612f91d8a8001a8bd898eefc4e507fd26
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-18 18:29:05 +0200

    Fix code style

--- src/aboutdialog/aboutpagecontents.cpp
+++ src/aboutdialog/aboutpagecontents.cpp
@@ -15,20 +15,25 @@
 using namespace Qt::Literals::StringLiterals;
 
 
-AboutPageGeneral::AboutPageGeneral(QWidget* parent)
+//
+// Authors
+
+AboutPageAuthors::AboutPageAuthors(QWidget* parent)
     : AboutPage{parent}
 {
-    const QStringList paragraphs = {
-        tr("<a href=\"https://mediathekview.de\" title=\"Visit MediathekView's homepage\">MediathekView</a> is a free open-source software designed to manage the online multimedia libraries of several German public broadcasters as well as an Austrian, a Swiss and a Franco-German public broadcaster."),
-        tr("<a href=\"https://rstammdev.github.io/mediathekview-qt\" title=\"Visit project's homepage\">MediathekView-Qt</a> is a free open-source frontend tool for this MediathekView database written in C++ using the Qt framework."),
-        tr("Copyright &copy; 2025 <a href=\"%2\" title=\"Visit organization's homepage\">%1</a>.").arg(QApplication::organizationName(), QApplication::organizationDomain()),
-        tr("This program is distributed under the terms of the <a href=\"https://www.gnu.org/licenses/gpl-3.0.en.html\" title=\"Visit license's homepage\">GNU General Public License</a>, either version 3 of the License, or any later version.")
+    const QList<QStringList> items = {
+        {
+            tr("Ralf Stamm"),
+            tr("Created and developed by <a href=\"https://rstammdev.github.io\" title=\"Visit contributor's homepage\">Ralf Stamm</a>.")
+        }
     };
 
-    QString text;
-    for (const QString& paragraph : paragraphs) {
-        text += tr("<p>%1</p>\n").arg(paragraph);
+    QString text = tr("<dl>");
+    for (QStringList item : items) {
+        text += tr("<dt><strong>%1</strong></dt>").arg(item[0]);
+        text += tr("<dd>%1</dd>").arg(item[1]);
     }
+    text += tr("</dl>");
 
     QLabel* content = new QLabel;
     content->setText(text);
@@ -42,9 +47,12 @@ AboutPageGeneral::AboutPageGeneral(QWidget* parent)
     layout->addStretch();
     setLayout(layout);
 
-    setPageTitle(tr("General"));
+    setPageTitle(tr("Authors"));
 }
 
+
+//
+// Components
 
 AboutPageComponents::AboutPageComponents(QWidget* parent)
     : AboutPage{parent}
@@ -100,38 +108,8 @@ QString AboutPageComponents::prettyPlatformName() const
 }
 
 
-AboutPageAuthors::AboutPageAuthors(QWidget* parent)
-    : AboutPage{parent}
-{
-    const QList<QStringList> items = {
-        {
-            tr("Ralf Stamm"),
-            tr("Created and developed by <a href=\"https://rstammdev.github.io\" title=\"Visit contributor's homepage\">Ralf Stamm</a>.")
-        }
-    };
-
-    QString text = tr("<dl>");
-    for (QStringList item : items) {
-        text += tr("<dt><strong>%1</strong></dt>").arg(item[0]);
-        text += tr("<dd>%1</dd>").arg(item[1]);
-    }
-    text += tr("</dl>");
-
-    QLabel* content = new QLabel;
-    content->setText(text);
-    content->setTextFormat(Qt::RichText);
-    content->setTextInteractionFlags(Qt::TextBrowserInteraction);
-    content->setOpenExternalLinks(true);
-    content->setWordWrap(true);
-
-    QVBoxLayout* layout = new QVBoxLayout;
-    layout->addWidget(content);
-    layout->addStretch();
-    setLayout(layout);
-
-    setPageTitle(tr("Authors"));
-}
-
+//
+// Credits
 
 AboutPageCredits::AboutPageCredits(QWidget* parent)
     : AboutPage{parent}
@@ -169,6 +147,43 @@ AboutPageCredits::AboutPageCredits(QWidget* parent)
     setPageTitle(tr("Credits"));
 }
 
+
+//
+// General
+
+AboutPageGeneral::AboutPageGeneral(QWidget* parent)
+    : AboutPage{parent}
+{
+    const QStringList paragraphs = {
+        tr("<a href=\"https://mediathekview.de\" title=\"Visit MediathekView's homepage\">MediathekView</a> is a free open-source software designed to manage the online multimedia libraries of several German public broadcasters as well as an Austrian, a Swiss and a Franco-German public broadcaster."),
+        tr("<a href=\"https://rstammdev.github.io/mediathekview-qt\" title=\"Visit project's homepage\">MediathekView-Qt</a> is a free open-source frontend tool for this MediathekView database written in C++ using the Qt framework."),
+        tr("Copyright &copy; 2025 <a href=\"%2\" title=\"Visit organization's homepage\">%1</a>.").arg(QApplication::organizationName(), QApplication::organizationDomain()),
+        tr("This program is distributed under the terms of the <a href=\"https://www.gnu.org/licenses/gpl-3.0.en.html\" title=\"Visit license's homepage\">GNU General Public License</a>, either version 3 of the License, or any later version.")
+    };
+
+    QString text;
+    for (const QString& paragraph : paragraphs) {
+        text += tr("<p>%1</p>\n").arg(paragraph);
+    }
+
+    QLabel* content = new QLabel;
+    content->setText(text);
+    content->setTextFormat(Qt::RichText);
+    content->setTextInteractionFlags(Qt::TextBrowserInteraction);
+    content->setOpenExternalLinks(true);
+    content->setWordWrap(true);
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->addWidget(content);
+    layout->addStretch();
+    setLayout(layout);
+
+    setPageTitle(tr("General"));
+}
+
+
+//
+// License
 
 AboutPageLicense::AboutPageLicense(QWidget* parent)
     : AboutPage{parent}
