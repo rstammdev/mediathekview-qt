2e18ba6719c50da53f49fb5f20b14f56f25f6959
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-08-24 18:57:57 +0200

    Add page header

--- src/settingsdialog/settingspageconfirmations.cpp
+++ src/settingsdialog/settingspageconfirmations.cpp
@@ -8,10 +8,30 @@
 
 #include "settingspageconfirmations.h"
 
+#include <QLabel>
+#include <QVBoxLayout>
+
 
 SettingsPageConfirmations::SettingsPageConfirmations(QWidget* parent)
     : SettingsPage{parent}
 {
+    QLabel* title = new QLabel(tr("<strong style=\"font-size: large;\">%1</strong>").arg(tr("Confirmations")));
+    QLabel* description = new QLabel(tr("Specify which confirmation dialogs should be shown."));
+
+
+
+    QVBoxLayout* layout = new QVBoxLayout;
+    layout->setContentsMargins(-1, -1, 0, 0);
+    layout->addWidget(title);
+    layout->addWidget(description);
+    layout->addStretch();
+
+    QWidget* widget = new QWidget(parent);
+    widget->setLayout(layout);
+
+    setWidget(widget);
+    setWidgetResizable(true);
+    setFrameShape(QFrame::NoFrame);
 
     setPageType(SettingsPage::PageTypeRoot);
     setPageTitle(tr("Confirmations"));
