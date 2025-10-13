37669a08ad5fc0de8f85b1573f37a02a14ea7b1b
rstammdev <219742789+rstammdev@users.noreply.github.com>
2025-10-13 20:50:03 +0200

    Create basic page 'Authors'

---
+++ src/aboutdialogpageauthors.cpp
@@ -0,0 +1,14 @@
+/**
+ * SPDX-FileComment: Project Homepage: https://github.com/rstammdev/mediathekview-qt
+ *
+ * SPDX-FileCopyrightText: 2025 Ralf Stamm
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#include "aboutdialogpageauthors.h"
+
+
+AboutDialogPageAuthors::AboutDialogPageAuthors(QWidget* parent)
+    : QWidget{parent}
+{}
